e866744bc477bc6aac1c942fcb28741d9c2bea13
Harsh Bhavsar <82403079+harsh18082001@users.noreply.github.com>
2024-09-23 01:47:37 +0530

    restart data

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 
 const middlewares = jsonServer.defaults();
+
 server.use(middlewares);
 server.use(router);
 server.listen(8000, () => { console.log('JSON Server is running'); })
